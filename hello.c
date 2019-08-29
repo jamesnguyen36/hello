6db806bd2d2983e1272b978d456f2d47a1ea664a
jamesnguyen36 <54559292+jamesnguyen36@users.noreply.github.com>
2019-08-29 08:49:07 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -3,8 +3,8 @@
 
 int main(int argc, char **argv) {
 
-  printf("James Nguyen\n", );
-  printf("Computer Science\n", );
+  printf("James Nguyen\n");
+  printf("Computer Science\n");
   printf("Kok Hwa Khor\n");
   printf("Electrical Engineering\n");
   
