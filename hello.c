31f15da7073b34a11cd903fb4fd0fb14807b061d
Kwa0915 <54672751+Kwa0915@users.noreply.github.com>
2019-08-29 08:46:31 -0500

    Added my name and major

--- hello.c
+++ hello.c
@@ -5,5 +5,8 @@ int main(int argc, char **argv) {
 
   printf("James Nguyen\n", );
   printf("Computer Science\n", );
+  printf("Kok Hwa Khor\n");
+  printf("Electrical Engineering\n");
+  
   return 0;
 }
